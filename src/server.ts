2aa141600acb81a36086863fcaf735413de961cc
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2021-06-27 00:30:51 -0300

    feat: stage two

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import 'reflect-metadata';
+
+import { router } from './routes';
+
+import './database';
 
 const app = express();
+app.use(express.json());
 
-app.listen(3000, () => console.log("Server is running! 🚀"));
+app.use(router);
+
+app.listen(3000, () => console.log('Server is running! 🚀'));
