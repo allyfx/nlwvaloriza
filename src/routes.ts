f5d3d7b82b64bdfdb476ac5ca7c06182dcf67160
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2021-06-27 12:48:28 -0300

    feat: stage three

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+
+import { ensureAdmin } from './middlewares/ensureAdmin';
+
 import { CreateUserController } from './controllers/CreateUserController';
+import { CreateTagController } from './controllers/CreateTagController';
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post('/users', createUserController.handle);
+
+router.post('/tags', ensureAdmin, createTagController.handle);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express from 'express';
 import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import { router } from './routes';
 
@@ -9,5 +10,17 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    error: 'Internal server error'
+  })
+});
 
 app.listen(3000, () => console.log('Server is running! 🚀'));
